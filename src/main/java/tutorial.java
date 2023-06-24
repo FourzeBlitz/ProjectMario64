2047e789aa568e55f47ac58485ec1e30fd301634
fourzeblitz <fourzeblitz@gmail.com>
2023-06-24 16:37:28 +0700

    ep 10, 3d obj rendering + 1 texture

--- src/main/java/tutorial.java
+++ src/main/java/tutorial.java
@@ -7,6 +7,7 @@ import org.lwjgl.opengl.GL;
 import org.lwjglx.util.vector.Vector3f;
 import renderEngine.Loader;
 import models.RawModel;
+import renderEngine.OBJLoader;
 import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
@@ -37,109 +38,12 @@ public class tutorial {
         Renderer renderer = new Renderer(shader, window);
 
         // init objects
-//        float[] vertices = {
-//                -0.5f, 0.5f, 0f,//v0
-//                -0.5f, -0.5f, 0f,//v1
-//                0.5f, -0.5f, 0f,//v2
-//                0.5f, 0.5f, 0f,//v3
-//        };
-//
-//        int[] indices = {
-//                0,1,3,//top left triangle (v0, v1, v3)
-//                3,1,2//bottom right triangle (v3, v1, v2)
-//        };
-//
-//        float[] textureCoords = {
-//                0,0, //v0
-//                0,1, //v1
-//                1,1, //v2
-//                1,0 //v3
-//        };
 
-        float[] vertices = {
-                -0.5f,0.5f,-0.5f,
-                -0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,-0.5f,
-                0.5f,0.5f,-0.5f,
-
-                -0.5f,0.5f,0.5f,
-                -0.5f,-0.5f,0.5f,
-                0.5f,-0.5f,0.5f,
-                0.5f,0.5f,0.5f,
-
-                0.5f,0.5f,-0.5f,
-                0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,0.5f,
-                0.5f,0.5f,0.5f,
-
-                -0.5f,0.5f,-0.5f,
-                -0.5f,-0.5f,-0.5f,
-                -0.5f,-0.5f,0.5f,
-                -0.5f,0.5f,0.5f,
-
-                -0.5f,0.5f,0.5f,
-                -0.5f,0.5f,-0.5f,
-                0.5f,0.5f,-0.5f,
-                0.5f,0.5f,0.5f,
-
-                -0.5f,-0.5f,0.5f,
-                -0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,0.5f
-
-        };
-
-        float[] textureCoords = {
-
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0
-
-
-        };
-
-        int[] indices = {
-                0,1,3,
-                3,1,2,
-                4,5,7,
-                7,5,6,
-                8,9,11,
-                11,9,10,
-                12,13,15,
-                15,13,14,
-                16,17,19,
-                19,17,18,
-                20,21,23,
-                23,21,22
-
-        };
-
-        RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+        RawModel model = OBJLoader.loadObjModel("resources/model/GameCube - Mario Superstar Baseball - Mario/Mario/mario.obj", loader);
         ModelTexture texture = new ModelTexture(loader.loadTexture("resources/model/GameCube - Mario Superstar Baseball - Mario/Mario/mario_1.png"));
         TexturedModel texturedModel = new TexturedModel(model, texture);
 
-        Entity entity = new Entity(texturedModel, new Vector3f(0,0,-5),0,0,0,1);
+        Entity entity = new Entity(texturedModel, new Vector3f(0,0,-10),0,0,0,1);
 
         Camera camera = new Camera(window);
 
@@ -149,7 +53,7 @@ public class tutorial {
             GL.createCapabilities();
 
             // game logic
-            entity.increaseRotation(1,1,0);
+            entity.increaseRotation(0,1,0);
             camera.move();
             renderer.prepare();
             shader.start();
